3ae1509bc98493db56ff7dfbc512e1fedfce3177
Collins <orumgbecollins5@gmail.com>
2026-04-17 14:48:28 +0100

    Add files via upload

---
+++ scripts/utils/search.js
@@ -0,0 +1,19 @@
+export function handleSearch() {
+  const searchInput = document.querySelector('.search-bar');
+  const searchQuery = searchInput.value.trim();
+  if (searchQuery) {
+    window.location.href = `index.html?search=${searchQuery}`;
+  }
+}
+  document.querySelector('.search-button').addEventListener('click', (e) => {
+    e.preventDefault();
+    handleSearch();
+  })
+
+  document.querySelector('.search-bar').addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') {
+      e.preventDefault();
+      handleSearch();
+    }
+
+  });
